f7cb1fbee7ce7d088b760bef97d05cbfaf745da7
William Casey <notwillcasey@gmail.com>
2021-07-24 13:33:59 -0600

    Initial Load with current state returns two question objects from API to client

--- client/src/components/questions_answers/QuestionsAnswers.jsx
+++ client/src/components/questions_answers/QuestionsAnswers.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import QuestionList from './QuestionList.jsx';
 
 class QuestionsAnswers extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      intial: []
+    };
+  }
+
+  componentDidMount () {
+    const url = window.location.href;
+    axios.get(`${url}atelier/initialQA`, {
+      method: 'GET',
+      params: {
+        product_id: this.props.id
+      }
+    })
+      .then((response) => {
+        this.setState({ initial: response.data });
+      });
   }
 
   render () {
     return (
       <div className='QA'>
         <div id='Qtitle'>Questions and Answers</div>
-        <div>{this.props.id}</div>
         <input id='Qsearch' type='text' placeholder='have a question? search for answers...'></input>
         <QuestionList />
+        <button>QA</button>
         </div>
     );
   }
--- server/routes/questions_answers.js
+++ server/routes/questions_answers.js
@@ -2,8 +2,25 @@ const router = require('express').Router();
 const axios = require('axios');
 const key = require('../config.js');
 
-// router.get('/initialQA', (req, res) => {
-
-// });
+router.get('/initialQA', (req, res) => {
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions', {
+    method: 'GET',
+    headers: {
+      Authorization: key.TOKEN
+    },
+    params: {
+      product_id: req.query.product_id,
+      page: 1,
+      amount: 2
+    }
+  })
+    .then((response) => {
+      res.json(response.data.results);
+      res.end();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
