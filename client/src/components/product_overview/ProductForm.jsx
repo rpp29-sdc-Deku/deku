0045007763c985285d8e76e837fd5de30d0d5faf
Salma Noe <sattari2101@gmail.com>
2021-07-29 17:41:46 -0700

    some progress

--- client/src/components/product_overview/ProductForm.jsx
+++ client/src/components/product_overview/ProductForm.jsx
@@ -1,33 +1,49 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 
 class ProductForm extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      value: 'SELECT SIZE'
+      size: '',
+      amount: '1'
     };
   }
 
-  handleChange (e) {
-    this.setState({ value: e.target.value });
+  handleSizeChange (e) {
+    this.setState({ size: e.target.value });
   }
 
-  handleSubmit (e) {
-    e.preventDefault();
-    alert('you picked the size ', this.state.value);
+  handleAmountChange (e) {
+    this.setState({ amount: e.target.value });
   }
+
+  // handleSubmit (e) {
+  //   e.preventDefault();
+  //   alert('you picked the size ', this.state.value);
+  // }
 
   render () {
     return (
       <div>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <select value={this.state.value} onChange={this.handleChange.bind(this)}>
-            <option value="XS">XS</option>
-            <option value="S">S</option>
-            <option value="M">M</option>
-            <option value="L">L</option>
-          </select>
-        </form>
+        <FormControl className="form">
+          <Select className="select select_size" onChange={this.handleSizeChange.bind(this)} displayEmpty value={this.state.size}>
+            <MenuItem value="" disabled>SELECT SIZE</MenuItem>
+            <MenuItem value="S">S</MenuItem>
+            <MenuItem value="M">M</MenuItem>
+            <MenuItem value="L">L</MenuItem>
+          </Select>
+          <Select className="select select_amount" onChange={this.handleAmountChange.bind(this)} value={this.state.amount}>
+            <MenuItem value="1">1</MenuItem>
+            <MenuItem value="2">2</MenuItem>
+            <MenuItem value="3">3</MenuItem>
+            <MenuItem value="4">4</MenuItem>
+          </Select>
+          <div className="add_to_bag">
+            ADD TO BAG
+          </div>
+        </FormControl>
       </div>
     );
   }
