23d293384a89c9870f3fed16c910821ddfeef0da
Richard Medina <medinarichard996@gmail.com>
2021-07-29 19:16:57 -0700

    Added some styling to review list

--- client/src/components/reviews/List.jsx
+++ client/src/components/reviews/List.jsx
@@ -7,20 +7,29 @@ class List extends React.Component {
     this.state = {};
   }
 
+  parseDate (date) {
+    const newDate = new Date(date);
+    const day = newDate.getDate().toString();
+    const month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(newDate).toString();
+    const year = newDate.getFullYear().toString();
+    return month + ' ' + day + ', ' + year;
+  }
+
   render () {
     return (
       <div className='reviewTile'>
-          <div className='reviewStarRaiting'>
-            starts review | {this.props.review.rating}
+          <div className='reviewStarRating'>
+            <h3>starts review | {this.props.review.rating}</h3>
+            <h3>{this.parseDate(this.props.review.date)}</h3>
           </div>
           <div className='reviewTitle'>
-            review title | {this.props.review.summary}
+            <h1>{this.props.review.summary}</h1>
           </div>
           <div className='reviewBody'>
-            review body | {this.props.review.body}
+            <p>{this.props.review.body}</p>
           </div>
           <div className='reviewHelpful'>
-            review helpful | {this.props.review.helpfulness}
+            <p>Helpful? <span>Yes</span>({this.props.review.helpfulness}) | <span>Report</span></p>
           </div>
           <div className='reviewResponse'>
             review response | {this.props.review.response}
