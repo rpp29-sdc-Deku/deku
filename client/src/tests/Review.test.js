e71b3d0f1e8a3c948fe883a784351aa69930d015
Richard Medina <medinarichard996@gmail.com>
2021-07-31 11:23:50 -0700

    Removed a function

--- client/src/tests/Review.test.js
+++ client/src/tests/Review.test.js
@@ -3,15 +3,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Ratings from '../components/reviews/Ratings.jsx';
 
-const getReviews = (productId, cb) => {
-  $.ajax({
-    type: 'GET',
-    url: '/atelier/reviews',
-    success: (results) => {
-      cb(results);
-    }
-  });
-};
 describe('Mock test', () => {
   it('Should equal true', () => {
     expect(2 + 2).toEqual(4);
