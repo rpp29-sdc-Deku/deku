4583d4af94b8220adf6261d0b07415d8e2a4ec0b
Chris Monteilh <chris@sunsationalswimschool.com>
2021-08-05 18:33:28 -0700

    load images for related products, refactor previous backend code

--- server/helpers/related_products_helpers.js
+++ server/helpers/related_products_helpers.js
@@ -7,38 +7,83 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 const getRelatedProducts = (productId) => {
-  return axios.get(`${keys.API}products/${productId}/related`, {
+  const relatedProductIds = axios.get(`${keys.API}products/${productId}/related`, {
     headers: {
       Authorization: keys.TOKEN
     }
   });
+
+  // need to retrievee for category, name, slogan, price
+  const relatedProductDetails = relatedProductIds.then(productIds => {
+    return Promise.all(productIds.data.map(id => {
+      return axios.get(`${keys.API}products/${id}`, {
+        headers: {
+          Authorization: keys.TOKEN
+        }
+      });
+    }));
+  });
+
+  const productImages = relatedProductDetails.then((productsList) => {
+    return Promise.all(productsList.map(product => {
+      return axios.get(`${keys.API}products/${product.data.id}/styles`, {
+        headers: {
+          Authorization: keys.TOKEN
+        }
+      });
+    }));
+  });
+
+  const productDetailsWithImages = productImages.then((images) => {
+    const needed = ['id', 'photos'];
+    const imageData = images.map(image => {
+      const id = image.data.product_id;
+      const imageObj = image.data.results[0];
+      imageObj.id = id;
+      const filtered = Object.keys(imageObj)
+        .filter(key => needed.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = imageObj[key];
+          return obj;
+        }, {});
+      return filtered;
+    });
+
+    // console.log('IMAGE DATA ', imageData);
+    return imageData;
+  });
+
+  // console.log('PRODUCT IMAGES ', productImages);
+
+  return Promise.all([relatedProductDetails, productDetailsWithImages]);
 };
 
-const getProductDetails = (productIds) => {
-  // console.log('PRODUCT IDS ARRAY IN HELPER ', productIds);
-  const productDetails = productIds.map(id => {
-    return axios.get(`${keys.API}products/${id}`, {
-      headers: {
-        Authorization: keys.TOKEN
-      }
-    });
+const combineDetailsAndImages = (productDetails, productImages) => {
+  const needed = ['id', 'name', 'slogan', 'default_price'];
+  const productOverview = productDetails.map(product => {
+    const raw = product.data;
+    const filtered = Object.keys(raw)
+      .filter(key => needed.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = raw[key];
+        return obj;
+      }, {});
+    return filtered;
   });
-  return productDetails;
-};
 
-const addProductImages = (productsList) => {
-  const productImages = productsList.map(product => {
-    return axios.get(`${keys.API}products/${product.id}/styles`, {
-      headers: {
-        Authorization: keys.TOKEN
-      }
-    });
+  const combinedData = productOverview.map((product, i) => {
+    const combine = {
+      ...product,
+      ...productImages[i]
+    };
+    return combine;
   });
-  return productImages;
+
+  // console.log('product images ============== ', combinedData);
+  return combinedData;
 };
 
 module.exports = {
   getRelatedProducts: getRelatedProducts,
-  getProductDetails: getProductDetails,
-  addProductImages: addProductImages
+  combineDetailsAndImages: combineDetailsAndImages
 };
