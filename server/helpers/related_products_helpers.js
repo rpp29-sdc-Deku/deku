e67e677bc163fa13e053451ca35f8ac659e592d7
Chris Monteilh <chris@sunsationalswimschool.com>
2021-08-19 16:57:47 -0700

    Fix intentional serverror. Install additional dependencies for Overview

--- server/helpers/related_products_helpers.js
+++ server/helpers/related_products_helpers.js
@@ -15,8 +15,8 @@ const getRelatedProducts = (productId) => {
     }
   });
 
+  // need to retrievee for category, name, slogan, price
   const relatedProductDetails = relatedProductIds.then(productIds => {
-    productIds.data.push(productId);
     return Promise.all(productIds.data.map(id => {
       return axios.get(`${apiURL}products/${id}`, {
         headers: {
@@ -27,23 +27,23 @@ const getRelatedProducts = (productId) => {
   });
 
   const productImages = relatedProductDetails.then((productsList) => {
-    productsList.map(product => {
+    return Promise.all(productsList.map(product => {
       return axios.get(`${apiURL}products/${product.data.id}/styles`, {
         headers: {
           Authorization: apiToken
         }
       });
-    });
+    }));
   });
 
   const productDetailsWithImages = productImages.then((images) => {
-    // const needed = ['id', 'photos'];
+    const needed = ['id', 'photos'];
     const imageData = images.map(image => {
       const id = image.data.product_id;
       const imageObj = image.data.results[0];
       imageObj.id = id;
       const filtered = Object.keys(imageObj)
-        // .filter(key => needed.includes(key))
+        .filter(key => needed.includes(key))
         .reduce((obj, key) => {
           obj[key] = imageObj[key];
           return obj;
@@ -61,7 +61,7 @@ const getRelatedProducts = (productId) => {
 };
 
 const combineDetailsAndImages = (productDetails, productImages) => {
-  const needed = ['id', 'name', 'slogan', 'default_price', 'category'];
+  const needed = ['id', 'name', 'slogan', 'default_price'];
   const productOverview = productDetails.map(product => {
     const raw = product.data;
     const filtered = Object.keys(raw)
@@ -70,7 +70,6 @@ const combineDetailsAndImages = (productDetails, productImages) => {
         obj[key] = raw[key];
         return obj;
       }, {});
-    // console.log('RAW details and images ========= ', filtered);
     return filtered;
   });
 
