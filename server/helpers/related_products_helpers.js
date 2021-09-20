293de1f90582925736f7d6a3bc26640dd2f5d998
Isaac <iketart2@gmail.com>
2021-09-20 15:55:12 -0700

    Deploy

--- server/helpers/related_products_helpers.js
+++ server/helpers/related_products_helpers.js
@@ -1,34 +1,24 @@
 const axios = require('axios');
 const router = require('express').Router();
 const bodyParser = require('body-parser');
-
 const apiToken = process.env.API_TOKEN;
 const apiURL = process.env.API;
-
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-
 const getRelatedProducts = (currentProductId) => {
-
-  //delete this variable and use incoming variable from line 11 into the end at line 15
+    //delete this variable and use incoming variable from line 11 into the end at line 15
   //hardcoded now while I want for other service to be up and running.
-
-  const currentProductIdtesting = -1;
-
-  //http://localhost:5000/api/related?_id=${currentProductIdtesting}
-
-  const relatedProductIds = axios.get(`http://18.212.20.132:5000/api/related?_id=-1`, {
+  const hardcode = -1;
+  //{ “_id” : “-1", “current_product_id” : -1, “related_product_id” : [ 47422, 47423, 47428, 47427 ] }
+  //local host ${apiURL}products/${currentProductId}/related
+  const relatedProductIds = axios.get(`http://54.151.28.108:5000/api/related?_id=${hardcode}`, {
     headers: {
       Authorization: apiToken
     }
   });
-
   // need to retrievee for category, name, slogan, price
   const relatedProductDetails = relatedProductIds.then(currentProductIds => {
-    let servercount = 0
     currentProductIds.data.push(currentProductId);
-    servercount++
-    console.log(servercount)
     return Promise.all(currentProductIds.data.map(id => {
       return axios.get(`${apiURL}products/${id}`, {
         headers: {
@@ -37,7 +27,6 @@ const getRelatedProducts = (currentProductId) => {
       });
     }));
   });
-
   const productImages = relatedProductDetails.then((productsList) => {
     return Promise.all(productsList.map(product => {
       return axios.get(`${apiURL}products/${product.data.id}/styles`, {
@@ -47,9 +36,8 @@ const getRelatedProducts = (currentProductId) => {
       });
     }));
   });
-
   const productDetailsWithImages = productImages.then((images) => {
-    // const needed = ['id', 'photos'];
+    // const needed = [‘id’, ‘photos’];
     const imageData = images.map(image => {
       const id = image.data.product_id;
       const imageObj = image.data.results[0];
@@ -62,18 +50,14 @@ const getRelatedProducts = (currentProductId) => {
         }, {});
       return filtered;
     });
-
-    // console.log('IMAGE DATA ', imageData);
+    // console.log(‘IMAGE DATA ’, imageData);
     return imageData;
   });
-
-  // console.log('PRODUCT IMAGES ', productImages);
-
+  // console.log(‘PRODUCT IMAGES ’, productImages);
   return Promise.all([relatedProductDetails, productDetailsWithImages]);
 };
-
 const combineDetailsAndImages = (productDetails, productImages) => {
-  // const needed = ['id', 'name', 'slogan', 'default_price', 'category'];
+  // const needed = [‘id’, ‘name’, ‘slogan’, ‘default_price’, ‘category’];
   const productOverview = productDetails.map(product => {
     const raw = product.data;
     const filtered = Object.keys(raw)
@@ -84,7 +68,6 @@ const combineDetailsAndImages = (productDetails, productImages) => {
       }, {});
     return filtered;
   });
-
   const combinedData = productOverview.map((product, i) => {
     const combine = {
       ...productImages[i],
@@ -92,11 +75,9 @@ const combineDetailsAndImages = (productDetails, productImages) => {
     };
     return combine;
   });
-
-  // console.log('product images ============== ', combinedData);
+  // console.log(‘product images ============== ’, combinedData);
   return combinedData;
 };
-
 module.exports = {
   getRelatedProducts: getRelatedProducts,
   combineDetailsAndImages: combineDetailsAndImages
