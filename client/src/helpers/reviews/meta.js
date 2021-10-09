2497c26684887c2a57f4b5e8e449b7feb85d5737
stevenharderjr <48738599+stevenharderjr@users.noreply.github.com>
2021-10-09 14:04:20 -0600

    Update meta.js

--- client/src/helpers/reviews/meta.js
+++ client/src/helpers/reviews/meta.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 const getMeta = (productId, cb) => {
   $.ajax({
     type: 'GET',
-    url: 'http://34.217.233.254:4000/atelier/reviews/meta',
+    url: 'http://35.82.3.48/atelier/reviews/meta',
     data: {
       product_id: productId
     },
